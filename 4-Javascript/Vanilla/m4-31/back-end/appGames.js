85c2ccb0bd6f3783111f06ce46394f1b68c31a1c
Vinicius Noronha <viniciusna633@outlook.com>
2022-02-16 12:20:53 -0300

    m5-4, 16/02-12h

--- 4-Javascript/Vanilla/m4-31/back-end/appGames.js
+++ 4-Javascript/Vanilla/m4-31/back-end/appGames.js
@@ -8,9 +8,29 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors())
 
+function register(data, newRegister) {
+    let datajson = JSON.parse(data)
+    datajson.push(newRegister)
+
+    return datajson
+}
+
+app.get('/', (req, res) => {
+    fs.readFile('./database/games/gamesdatabase.json', (err, data) => {
+        if (err) throw err;
+        res.send(data)
+    })
+})
+
 app.post('/', (req, res) =>{
-    res.sendStatus(200)
-    console.log(req.body)
+    res.sendStatus(201)
+
+    fs.readFile('./database/games/gamesdatabase.json', (err, data) => {
+        if (err) throw err;
+        fs.writeFile('./database/games/gamesdatabase.json', JSON.stringify(register(data, req.body)), (err) => {
+            if (err) throw err;
+            console.log("escreveu")})
+    })
 })
 
 app.listen(3001)
